6fa5f9fc461fea770a2343f52e524f5a3f7c1499
chatme <dinukamalith08@gmail.com>
2025-04-13 15:32:16 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,11 +1,10 @@
-const GITHUB_USERNAME = "chatme1";
-const GITHUB_AUTH_TOKEN = "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"; // your token
-const BOT_NUMBER = "94788185692"; // your number
-const SESSION_ID = "PRABATH-MD~7Ac3VIQZ#pslGIGglMqrTKLPlk9_9MCszVbJUfT2EBeCdCMmVpzM"; // from Baileys or other auth
+const GITHUB_AUTH_TOKEN = "null"
+const BOT_NUMBER = "null"
+const SESSION_ID = ""
+
 
 module.exports = {
-  GITHUB_USERNAME,
-  GITHUB_AUTH_TOKEN,
-  BOT_NUMBER,
-  SESSION_ID
+GITHUB_AUTH_TOKEN,
+BOT_NUMBER,
+SESSION_ID
 }
