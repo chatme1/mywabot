bfca76d7a2446ff6f62d3abfbe7f88da8c798967
chatme <dinukamalith08@gmail.com>
2025-04-13 11:37:48 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94788185692"
+const SESSION_ID = "PRABATH-MD~7Ac3VIQZ#pslGIGglMqrTKLPlk9_9MCszVbJUfT2EBeCdCMmVpzM"
 
 
 module.exports = {
